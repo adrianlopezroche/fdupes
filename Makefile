adrianlopezroche/fdupes

commit 54c920e06ea5c6550b8fade9bfee8f3cb60a389f

fdupes-1.50-PR2

Makefile
@@ -14,25 +14,17 @@
 PREFIX = /usr/local
 
 #
-# To use the md5sum program for calculating signatures (instead of the
-# built in MD5 message digest routines) uncomment the following
-# line (try this if you're having trouble with built in code).
-#
-#EXTERNAL_MD5 = -DEXTERNAL_MD5=\"md5sum\"
-
-#
 # Certain platforms do not support long options (command line options).
 # To disable long options, uncomment the following line.
 #
 #OMIT_GETOPT_LONG = -DOMIT_GETOPT_LONG
 
 #
-# This version of fdupes can use a red-black tree structure to
-# store file information. This is disabled by default, as it
-# hasn't been optimized or verified correct. If you wish to
-# enable this untested option, uncomment the following line.
+# To use the md5sum program for calculating signatures (instead of the
+# built in MD5 message digest routines) uncomment the following
+# line (try this if you're having trouble with built in code).
 #
-#EXPERIMENTAL_RBTREE = -DEXPERIMENTAL_RBTREE
+#EXTERNAL_MD5 = -DEXTERNAL_MD5=\"md5sum\"
 
 #####################################################################
 # Developer Configuration Section                                   #
@@ -41,7 +33,7 @@ PREFIX = /usr/local
 #
 # VERSION determines the program's version number.
 #
-VERSION = 1.50-PR1
+include Makefile.inc/VERSION
 
 #
 # PROGRAM_NAME determines the installation name and manual page name
@@ -69,14 +61,12 @@ MAN_EXT = 1
 INSTALL = install	# install : UCB/GNU Install compatiable
 #INSTALL = ginstall
 
-GNU_TAR = tar		# tar : GNU Tar 
-#GNU_TAR = gtar
-
 RM      = rm -f
 
 MKDIR   = mkdir -p
 #MKDIR   = mkdirhier 
 #MKDIR   = mkinstalldirs
+
 
 #
 # Make Configuration
@@ -114,14 +104,10 @@ install: fdupes installdirs
 	$(INSTALL_PROGRAM)	fdupes   $(BIN_DIR)/$(PROGRAM_NAME)
 	$(INSTALL_DATA)		fdupes.1 $(MAN_DIR)/$(PROGRAM_NAME).$(MAN_EXT)
 
-tarball: clean
-	$(GNU_TAR) --directory=.. -c -z -v \
-                   -f ../fdupes-$(VERSION).tar.gz fdupes-$(VERSION)
-
 clean:
 	$(RM) $(OBJECT_FILES)
 	$(RM) fdupes
-	$(RM) *~
+	$(RM) *~ md5/*~
 
 love:
 	@echo You\'re not my type. Go find a human partner.

CONTRIBUTORS
@@ -4,8 +4,9 @@ on their contributions. Names are listed in alphabetical order.
 
  [AB] Adrian Bridgett		(adrian.bridgett@iname.com)
  [AL] Adrian Lopez 		(adrian2@caribe.net)
+[CHL] Charles Longeau		(chl@tuxfamily.org)
  [FD] Frank DENIS, a.k.a. 
-      Jedi/Sector One, a.k.a.
+      Jedi/Sector One, a.k.a. 
       DJ Chrysalis 		(j@4u.net)
  [KK] Kresimir Kukulj 		(madmax@pc-hrvoje.srce.hr)
  [LB] Laurent Bonnaud		(Laurent.Bonnaud@iut2.upmf-grenoble.fr)

Makefile.inc/VERSION
@@ -1,0 +1,5 @@
+#
+# VERSION determines the program's version number.
+#
+
+VERSION = 1.50-PR2

TODO
@@ -1,5 +1,72 @@
-- Add option to highlight or identify symlinked files.
-- Verify correctness of red-black tree algorithm. Optimize.
+- A bug with -S shows wrong results.
+
+- A bug causes the following behavior:
+
+  $ fdupes --symlinks testdir
+  testdir/with spaces b
+  testdir/with spaces a
+
+  testdir/zero_b
+  testdir/zero_a
+
+  testdir/symlink_two
+  testdir/twice_one
+
+  $ cp testdir/two testdir/two_again
+  $ fdupes --symlinks testdir
+  testdir/two_again
+  testdir/two
+  testdir/twice_one
+  testdir/symlink_two
+
+  testdir/with spaces b
+  testdir/with spaces a
+
+  testdir/zero_b
+  testdir/zero_a
+
+  ** This is not the desired behavior. Likewise:
+
+  $ fdupes testdir
+  testdir/with spaces b
+  testdir/with spaces a
+
+  testdir/zero_b
+  testdir/zero_a
+
+  testdir/twice_one
+  testdir/two
+
+  $ fdupes --symlinks testdir
+  testdir/with spaces b
+  testdir/with spaces a
+
+  testdir/zero_b
+  testdir/zero_a
+
+  testdir/symlink_two
+  testdir/twice_one
+
+- Don't assume that stat always works.
+
+- Add partial checksumming where instead of MD5ing whole
+  files we MD5 and compare every so many bytes, caching
+  these partial results for subsequent comparisons.
+
+- Option -R should not have to be separated from the rest,
+  such that "fdupes -dR testdir", "fdupes -d -R testdir",
+  "fdupes -Rd testdir", etc., all yield the same results.
+
+- Add option to highlight or identify symlinked files (suggest
+  using --classify to identify symlinks with @ suffix... when
+  specified, files containing @ are listed using \@).
+
+- Consider autodeletion option without user intervention.
+
+- Consider option to match only to files in specific directory.
+
+- Do a little commenting, to avoid rolling eyes and/or snickering.
+
 - Fix problem where MD5 collisions will result in one of the
   files not being registered (causing it to be ignored).
 

fdupes.1
@@ -52,6 +52,10 @@ prompt user for files to preserve, deleting all others (see
 .B CAVEATS
 below)
 .TP
+.B -N --noprompt
+when used together with --delete, preserve the first file in each
+set of duplicates and delete the others without prompting the user 
+.TP
 .B -v --version
 display fdupes version
 .TP

md5/md5.c
@@ -33,12 +33,17 @@
   <ghost@aladdin.com>.  Other authors are noted in the change history
   that follows (in reverse chronological order):
 
+  contributors
+   chl - Charles Longeau <chl@tuxfamily.org>
+
+  2002-05-31 chl Relocated string.h to avoid memcpy warning. 
   1999-11-04 lpd Edited comments slightly for automatic TOC extraction.
   1999-10-18 lpd Fixed typo in header comment (ansi2knr rather than md5).
   1999-05-03 lpd Original version.
  */
 
 #include "md5.h"
+#include <string.h>
 
 #ifdef TEST
 /*
@@ -46,7 +51,6 @@
  * The test program should print out the same values as given in section
  * A.5 of RFC 1321, reproduced below.
  */
-#include <string.h>
 main()
 {
     static const char *const test[7] = {

commit 2b250e8d2d74461bf855683b04d2de9085964ccf

Remove the dead and broken EXTERNAL_MD5.
It was broken in 54c920e06ea5c6550b8fade9bfee8f3cb60a389f (fdupes-1.50-PR2):
fdupes.c:531: undefined reference to `getcrcpartialsignature'
fdupes.c:546: undefined reference to `getcrcpartialsignature'

Seing as noone cared to fix it for seven years I'm removing the dead broken code.

Makefile
@@ -25,13 +25,6 @@ FILEOFFSET_64BIT = -D_FILE_OFFSET_BITS=64
 #
 #OMIT_GETOPT_LONG = -DOMIT_GETOPT_LONG
 
-#
-# To use the md5sum program for calculating signatures (instead of the
-# built in MD5 message digest routines) uncomment the following
-# line (try this if you're having trouble with built in code).
-#
-#EXTERNAL_MD5 = -DEXTERNAL_MD5=\"md5sum\"
-
 #####################################################################
 # Developer Configuration Section                                   #
 #####################################################################
@@ -80,7 +73,7 @@ MKDIR   = mkdir -p
 CC ?= gcc
 COMPILER_OPTIONS = -Wall -O -g
 
-CFLAGS= $(COMPILER_OPTIONS) -I. -DVERSION=\"$(VERSION)\" $(EXTERNAL_MD5) $(OMIT_GETOPT_LONG) $(FILEOFFSET_64BIT)
+CFLAGS= $(COMPILER_OPTIONS) -I. -DVERSION=\"$(VERSION)\" $(OMIT_GETOPT_LONG) $(FILEOFFSET_64BIT)
 
 INSTALL_PROGRAM = $(INSTALL) -c -m 0755
 INSTALL_DATA    = $(INSTALL) -c -m 0644

fdupes.c
@@ -33,9 +33,7 @@
 #include <errno.h>
 #include <libgen.h>
 
-#ifndef EXTERNAL_MD5
 #include "md5/md5.h"
-#endif
 
 #define ISFLAG(a,b) ((a & b) == b)
 #define SETFLAG(a,b) (a |= b)
@@ -345,10 +343,6 @@ int grokdir(char *dir, file_t **filelistp)
   return filecount;
 }
 
-#ifndef EXTERNAL_MD5
-
-/* If EXTERNAL_MD5 is not defined, use L. Peter Deutsch's MD5 library. 
- */
 char *getcrcsignatureuntil(char *filename, off_t max_read)
 {
   int x;
@@ -409,49 +403,6 @@ char *getcrcpartialsignature(char *filename)
 {
   return getcrcsignatureuntil(filename, PARTIAL_MD5_SIZE);
 }
-
-#endif /* [#ifndef EXTERNAL_MD5] */
-
-#ifdef EXTERNAL_MD5
-
-/* If EXTERNAL_MD5 is defined, use md5sum program to calculate signatures.
- */
-char *getcrcsignature(char *filename)
-{
-  static char signature[256];
-  char *command;
-  char *separator;
-  FILE *result;
-
-  command = (char*) malloc(strlen(filename)+strlen(EXTERNAL_MD5)+2);
-  if (command == NULL) {
-    errormsg("out of memory\n");
-    exit(1);
-  }
-
-  sprintf(command, "%s %s", EXTERNAL_MD5, filename);
-
-  result = popen(command, "r");
-  if (result == NULL) {
-    errormsg("error invoking %s\n", EXTERNAL_MD5);
-    exit(1);
-  }
- 
-  free(command);
-
-  if (fgets(signature, 256, result) == NULL) {
-    errormsg("error generating signature for %s\n", filename);
-    return NULL;
-  }    
-  separator = strchr(signature, ' ');
-  if (separator) *separator = '\0';
-
-  pclose(result);
-
-  return signature;
-}
-
-#endif /* [#ifdef EXTERNAL_MD5] */
 
 void purgetree(filetree_t *checktree)
 {